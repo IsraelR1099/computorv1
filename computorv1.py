16cb5dc2925ecca5ad1a3b0723aa5c113b918faf
IsraelR1099 <rifarachi10@gmail.com>
2024-12-28 22:58:01 +0100

    adding standalone coefficients and float-point numbers, combining sides

--- computorv1.py
+++ computorv1.py
@@ -4,15 +4,36 @@ import re
 
 
 def check_syntax(expression):
-    user_input = expression.split()
-    for exp in user_input:
-       print(f"input: {exp}")
-    sys.exit(0)
+    try:
+        left, right = expression.split('=')
+    except ValueError:
+        print("Error: invalid input")
+        sys.exit(1)
+
+    def check_side(side):
+        ret = re.fullmatch(
+            r'^([-+]?\d*(\.\d+)?\*?[xX](\^\d+)?|[-+]?\d+(\.\d+)?)([-+]\d*(\.\d+)?\*?[xX](\^\d+)?|[-+]?\d+(\.\d+)?)*$',
+            side.replace(' ', '').replace('**', '^'))
+        if not ret:
+            print("Error: invalid input")
+            sys.exit(1)
+        return side
+    left = check_side(left)
+    right = check_side(right)
+    if 'x' not in left.lower() and 'x' not in right.lower():
+        print(
+            "Error: invalid input - equation must at least one variable term"
+        )
+        sys.exit(1)
+    return expression
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Error: wrong number of arguments")
-        sys.exit (1)
+        sys.exit(1)
     valid_input = check_syntax(sys.argv[1])
-    polynomial = parser.parse(valid_input);
+    left_side, right_side = parser.parse(valid_input)
+    print(f"left side: {left_side}")
+    print(f"right side: {right_side}")
+    parser.combine_sides(left_side, right_side)
--- parser.py
+++ parser.py
@@ -2,32 +2,40 @@ import re
 
 
 def parse(string):
-    print(f"input is: {string}")
     left_side, right_side = string.split('=')
 
     def parse_side(expression):
-        print(f"expression in parse side {expression}")
-        criterium = re.compile(r'([-+]?\d*\.*\d*)\s*\*?\s*(x?)\^?(\d*)', re.IGNORECASE)
+        criterium = re.compile(
+            r'([-+]?\d*\.*\d*)\s*\*?\s*(x?)\^?(\d*)', re.IGNORECASE)
         poly = {}
-        signs = {'+': 1, '-': -1}
+        # signs = {'+': 1, '-': -1}
         for c, x, p, in criterium.findall(expression.replace(' ', '').replace('**', '^')):
             if not any((c, x, p)):
                 continue
-            print(f"p is: {p}")
             if c in ('+', '-'):
                 coeff = 1.0 if c == '+' else -1.0
             else:
                 coeff = float(c) if '.' in c else int(c or 1)
             # coeff = signs[c] if c in ('+', '-') else int(c or 1)
             power = int(p or 1) if x else 0
-            print(f"coeff is {coeff} and power {power}")
             if power in poly:
+                print(f"Same power, summing {poly[power]} and {coeff}")
                 poly[power] += coeff
                 continue
             poly[power] = coeff
-        print(f"poly is {poly}")
         return poly
     left_poly = parse_side(left_side)
-    print(f"left poly is: {left_poly}")
+    # print(f"left poly is: {left_poly}")
     right_poly = parse_side(right_side)
-    print(f"right poly is: {right_poly}")
+    # print(f"right poly is: {right_poly}")
+    return left_poly, right_poly
+
+
+def combine_sides(left_side, right_side):
+    for power in right_side:
+        if power in left_side:
+            left_side[power] -= right_side[power]
+        else:
+            left_side[power] = -right_side[power]
+    print(f"left side: {left_side}")
+    return left_side
